98d86a3d88044d17b3194fecc88af8ae1edbe269
Renz2299 <karenza.kv@outlook.com>
2023-08-07 08:02:01 +0100

    Guide page created
    Guide page template created, styling still needs finishing

--- assets/js/oxford.js
+++ assets/js/oxford.js
@@ -4,7 +4,13 @@ onload = function() {
 
 let pos;
 let map;
-let marker;
+let mapBounds;
+let infoWindow;
+let currentInfoWindow;
+let service;
+let infoPanel;
+let infoPanelPhoto;
+let infoPanelDetails;
 
 function initMap() {
     pos = {lat: 51.752022, lng: -1.257726};
@@ -13,60 +19,121 @@ function initMap() {
         zoom: 11
     });
 
-    // Lye Valley Path
-    addMarker({
-        coords:{lat: 51.745060, lng: -1.208960},
-        iconImage: 'assets/img/hiking2.png',
-        title: '<h3>Lye Valley Path</h3>',
-        rating: '<p>Rating: 4.8</p>'
-    });
-    // Wolvercote Goose Green
-    addMarker({
-        coords:{lat: 51.7864, lng: -1.2864},
-        iconImage: 'assets/img/hiking2.png',
-        title: '<h3>Wolvercote Goose Green</h3>',
-        rating: '<p>Rating: 4.3</p>'
-    });
-    // RSPB Otmoor
-    addMarker({
-        coords:{lat: 51.809871, lng: -1.174442},
-        iconImage: 'assets/img/forest2.png',
-        title: '<h3>RSPB Otmoor</h3>',
-        rating: '<p>Rating: 4.7</p>'
-    })
-    // Chilswell Valley
-    addMarker({
-        coords:{lat: 51.728798, lng: -1.277156},
-        iconImage: 'assets/img/forest2.png',
-        title: '<h3>Chilswell Valley</h3>',
-        rating: '<p>Rating: 4.7</p>'
-    })
-    // The Watcher Tree
-    addMarker({
-        coords:{lat: 51.75071, lng: -1.249208},
-        iconImage: 'assets/img/tower.png',
-        title: '<h3>The Watcher Tree</h3>',
-        rating: '<p>Rating: 3.6</p>'
-    })
-    // Devil's Quoits
-    addMarker({
-        coords:{lat: 51.73505, lng: -1.407093},
-        iconImage: 'assets/img/tower.png',
-        title: "<h3>Devil's Quoits</h3>",
-        rating: '<p>Rating: 4.3</p>'
-    })
+    mapBounds = new google.maps.LatLngBounds();
+    infoWindow = new google.maps.InfoWindow;
+    currentInfoWindow = infoWindow;
+
+    infoPanel = document.getElementById('guide-card');
+    infoPanelPhoto = document.getElementById('photo');
+    infoPanelDetails = document.getElementById('details');
+
+    getNearbyPlaces(pos);
 }
 
-function addMarker(props) {
-    marker = new google.maps.Marker ({
-    position: props.coords,
-    map: map,
-    icon: props.iconImage
+function getNearbyPlaces(position) {
+    let request = {
+        location: position,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        keyword: 'landmark'
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, nearbyCallback);
+}
+
+function nearbyCallback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        createMarkers(results);
+    }
+}
+
+function createMarkers(places) {
+    places.forEach(place => {
+        let marker = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map,
+            title: place.name
+        });
+
+        google.maps.event.addListener(marker, 'click', () => {
+            let request = {
+                placeId: place.place_id,
+                fields: ['name', 'formatted_address', 'geometry', 'rating', 'website', 'photos']
+            };
+
+            service.getDetails(request, (placeResult, status) => {
+                showDetails(placeResult, marker, status)
+            });
+        });
+
+        mapBounds.extend(place.geometry.location);
     });
-    let infoWindow = new google.maps.InfoWindow({
-        content: props.title + props.rating
-    });
-    marker.addListener('click', function(){
-        infoWindow.open(map, marker);
-    });
+
+    map.fitBounds(mapBounds);
+}
+
+function showDetails(placeResult, marker, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      let placeInfowindow = new google.maps.InfoWindow();
+      placeInfowindow.setContent('<div class="info-window"><strong>' + placeResult.name + '</strong><br>' + 'Rating: ' + placeResult.rating + '</div>');
+      placeInfowindow.open(marker.map, marker);
+      currentInfoWindow.close();
+      currentInfoWindow = placeInfowindow;
+      showPanel(placeResult);
+    } else {
+      console.log('showDetails failed:' + status);
+    }
+}
+
+function showPanel(placeResult) {
+    // If infoPanel is already open, close it
+    if (infoPanel.classList.contains("open")) {
+      infoPanel.classList.remove("open");
+    }
+  
+    // Clear previous details
+    while (infoPanelPhoto.lastChild) {
+        infoPanelPhoto.removeChild(infoPanelPhoto.firstChild);
+    }
+    while (infoPanelDetails.hasChildNodes()) {
+        infoPanelDetails.removeChild(infoPanelDetails.firstChild);
+    }
+
+    // Add primary photo, if there is one
+    if (placeResult.photos != null) {
+      let firstPhoto = placeResult.photos[0];
+      let photo = document.createElement('img');
+      photo.classList.add('hero');
+      photo.src = firstPhoto.getUrl();
+      infoPanelPhoto.appendChild(photo);
+    }
+  
+    // Add place details with formatting
+    let name = document.createElement('h2');
+    name.classList.add('place');
+    name.textContent = placeResult.name;
+    infoPanelDetails.appendChild(name);
+    if (placeResult.rating != null) {
+      let rating = document.createElement('p');
+      rating.classList.add('details');
+      rating.textContent = `Rating: ${placeResult.rating} \u272e`;
+      infoPanelDetails.appendChild(rating);
+    }
+    let address = document.createElement('p');
+    address.classList.add('details');
+    address.textContent = placeResult.formatted_address;
+    infoPanelDetails.appendChild(address);
+    if (placeResult.website) {
+      let websitePara = document.createElement('p');
+      let websiteLink = document.createElement('a');
+      let websiteUrl = document.createTextNode(placeResult.website);
+      websiteLink.appendChild(websiteUrl);
+      websiteLink.title = placeResult.website;
+      websiteLink.href = placeResult.website;
+      websitePara.appendChild(websiteLink);
+      infoPanelDetails.appendChild(websitePara);
+    }
+  
+    // Open the infoPanel
+    infoPanel.classList.add("open");
 }
